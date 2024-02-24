2ef4058a4247e18f5e62b902a6eb94bf6312275b
TheNequiz8 <157914659+TheNequiz8@users.noreply.github.com>
2024-02-23 18:20:31 -0600

    Delete conexion.php

--- conexion.php
+++
@@ -1,9 +0,0 @@
-<?php 
- $servidor="localhost";
- $ususario="root";
- $password="";
- $bd="biblioteca_virtual";
- $conexion=mysqli_connect($servidor,$ususario,$password,$bd)or die(mysql_error());
-
- 
- ?>
